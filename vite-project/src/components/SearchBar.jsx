6a5b8f98047986c9c0951d0c26e1ee98dec5a0da
Adel <@ariwidaniadelia@gmail.com>
2023-09-27 12:16:36 +0800

    edit HomePage, Searchbar, dan install react-icons

--- vite-project/src/components/SearchBar.jsx
+++ vite-project/src/components/SearchBar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { PropTypes } from 'prop-types';
 
-function SearchBar({ keyword, changeKeyword }) {
+function SearchBar({ keyword, keywordChange }) {
   return(
     <input
       className="search-bar"
       type="text"
       placeholder="Cari berdasarkan nama"
       value={keyword}
-      onChange={(event) => changeKeyword(event.target.value)}
+      onChange={(event) => keywordChange(event.target.value)}
     ></input>
   )
 }
 SearchBar.propTypes = {
   keyword: PropTypes.string.isRequired,
-  changeKeyword: PropTypes.func.isRequired
+  keywordChange: PropTypes.func.isRequired
 }
 
 export default SearchBar;
